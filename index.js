a583c4f560a47c08ce12ec9396e5a5fd800d0530
sheikh-alamin <mimoalamin@gmail.com>
2026-03-07 06:06:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById ("sign-in-btn").addEventListener("click" , function () {
+    const userName = document.getElementById("input-id");
+    const userId = userName.value;
+
+    const userPass = document.getElementById("input-pass");
+    const loginPass = userPass.value;
+
+    if (userId == "admin" && loginPass == "admin123") {
+        window.location.assign("./home.html")
+    }
+    else {
+        alert("login failed");
+        return;
+    }
+})
