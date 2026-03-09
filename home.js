e9be3ff9d9e7c7d79cb1ba1753e8d469015d3c1b
sheikh-alamin <mimoalamin@gmail.com>
2026-03-09 08:54:00 +0600

    spinner added

--- home.js
+++ home.js
@@ -24,8 +24,95 @@ const issuesCountElement = document.getElementById("issuesCount");
 //     displayIssues(openIssues);
 //     console.log(openIssues);
 // }
+const loadCardDetail = async (id) => {
+    const url= `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+    
+    const res = await fetch(url);
+    const details = await res.json();
+    displayCardDetails(details.data);
+}
+
+// "id": 50,
+// "title": "Create automated testing pipeline",
+// "description": "Set up CI/CD pipeline with automated tests running on every commit and pull request.",
+// "status": "open",
+// "labels": [
+// "enhancement",
+// "help wanted"
+// ],
+// "priority": "high",
+// "author": "ci_cd_cindy",
+// "assignee": "test_tina",
+// "createdAt": "2024-02-10T08:00:00Z",
+// "updatedAt": "2024-02-10T08:00:00Z" 
+const displayCardDetails = (card) => {
+    // console.log(card);
+    const detailsBox = document.getElementById("details-container");    
+
+    const labelStyles = {
+            BUG: "border-[#FECACA] bg-[#FEECEC] text-[#EF4444] ",
+            "HELP WANTED": "border-[#FDE68A] bg-[#FFF8DB] text-[#D97706]",
+            ENHANCEMENT: "border-[#A7F3D0] bg-[#ECFDF5] text-[#059669]",
+            "GOOD FIRST ISSUE": "border-blue-300 bg-blue-100 text-blue-700",
+        };
+
+    const labelIcons = {
+        BUG:"fa-solid fa-bug",
+        "HELP WANTED":"fa-solid fa-life-ring",
+        ENHANCEMENT:"fa-solid fa-wand-magic-sparkles",
+        "GOOD FIRST ISSUE": "fa-solid fa-tag",
+    };
+
+    const tagsHTML = (card.labels || [])
+        .map(label => {
+                const style = labelStyles[label.toUpperCase()] || "border-grey-300 bg-grey-200 text-grey-700";
+                const icon = labelIcons[label.toUpperCase()] || "fa-solid fa-tag" ;
+                return `<button class="py-1 px-4 border-1 uppercase rounded-2xl ${style}"><i class="${icon}"></i> ${label}</button>`;
+            })
+        .join("");
+
+    detailsBox.innerHTML  =  `
+                <h1 class="text-2xl font-bold">${card.title}</h1>
+                <div class="flex gap-1">
+                    <div class="badge badge-success">${card.status}</div>
+                    <p>.</p>
+                    <p>Opened by ${card.author}</p>
+                    <p>.</p>
+                    <p>${card.createdAt}</p>
+                </div>
+                <div class="flex gap-2">
+                    ${tagsHTML}
+                </div>
+                <p>${card.description}</p>
+                <div class="grid grid-cols-2 gap-2">
+                    <div>
+                        <p>Assignee:</p>
+                        <h3 class="font-bold">${card.assignee}</h3>
+                    </div>
+                    <div>
+                        <p>Priority:</p>
+                        <div class="badge badge-error">${card.priority}</div>
+                    </div>
+                </div>       
+
+    `;
+    document.getElementById("detail_modal").showModal();
+};
+
+const manageSpinner = (status) => {
+    if ( status == true ) {
+        document.getElementById("spinner").classList.remove("hidden");
+        document.getElementById("cardsContainer").classList.add("hidden");
+    }
+    else {
+        document.getElementById("cardsContainer").classList.remove("hidden");
+        document.getElementById("spinner").classList.add("hidden");
+
+    }
+}
 
 async function loadCards() {
+    manageSpinner(true);
     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
     const data = await res.json();
     displayCards(data.data);
@@ -72,7 +159,7 @@ function displayCards(cards) {
         .join("");
 
     cardElement.innerHTML = `
-                    <div class="p-4 space-y-3 ">
+                    <div  onclick="loadCardDetail(${card.id})" class="p-4 space-y-3">
                         <div class="flex justify-between items-center">
                             <img src="./assets/Open-Status.png" alt="">
                             <button class="py-1 px-4 bg-[#FEECEC] uppercase rounded-2xl text-[#EF4444]">${card.priority}</button>
@@ -84,7 +171,7 @@ function displayCards(cards) {
                         </div>
                     </div>
                     <hr style="border: 1px solid lightgrey;">
-                    <div class="p-4">
+                    <div onclick="loadCardDetail(${card.id})" class="p-4">
                         <p class="text-[#64748B] text-3"># ${card.id} by ${card.author}</p>
                         <p class="text-[#64748B] text-3">${card.createdAt}</p>
                     </div>
@@ -92,6 +179,8 @@ function displayCards(cards) {
     cardsContainer.appendChild(cardElement);
 
     });
+
+    manageSpinner(false);
 }
 
 loadCards();
